ec3113f0712b2fccb4300645f7ab6539301db5be
khushil-225844 <khushil.vekariya@gmail.com>
2026-01-19 12:37:37 +0300

    new

--- src/App.java
+++ src/App.java
@@ -1,5 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!1");
+
+        System.out.println("Hello, Khushil!1");
+    
+        String name = "Khushil" ;
+        int a, b, c ;
+        double price1 = 123.147 ;
+        // float price2 = 123.147
     }
+
 }
